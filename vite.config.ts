83e6adf2501bb59f51a77db02e03276bf3c17edb
User <user@example.com>
2026-06-21 00:54:39 +0700

    update base path to /stratos/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  base: "/redbull-stratos-web/",
+  base: "/stratos/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
